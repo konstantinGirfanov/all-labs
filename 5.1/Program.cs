ae41031ca5e29a2f3e20674c9640cab6ae7fe08b
Kostya <super_serega_2007@mail.ru>
2022-11-23 22:09:46 +0500

    5.1

--- 5.1/Program.cs
+++ 5.1/Program.cs
@@ -6,32 +6,31 @@ namespace Hello
     {
         public static void Main()
         {
-            float[] numbers = new float[2];
+            float previousNum = -1f;
+            float currentNum = 0f;
+            bool isFirstInput = true;
+
             while (true)
             {
                 Console.Write("Введите число: ");
-                var a = Console.ReadLine();
+                var userInput = Console.ReadLine();
 
-                if (int.TryParse(a, out int b))
+                if (isFirstInput)
                 {
-                    Console.WriteLine(b.ToString());
-                    continue;
+                    isFirstInput = false;
                 }
-
                 else
                 {
-                    if (numbers[0] == 0)
-                        float.TryParse(a, out numbers[0]);
-                    else if (numbers[0] != 0 && numbers[1] == 0)
-                        float.TryParse(a, out numbers[1]);
-                    else if (numbers[0] != 0 && numbers[1] != 0 && float.TryParse(a, out _))
-                    {
-                        numbers[0] = numbers[1];
-                        float.TryParse(a, out numbers[1]);
-                    }
+                    previousNum = currentNum;
+                }
+                currentNum = float.Parse(userInput);
+
+                if (int.TryParse(userInput, out int b))
+                {
+                    Console.WriteLine(b.ToString());
                 }
 
-                if (numbers[0] == numbers[1] || a == "q") break;
+                if (Math.Abs(previousNum - currentNum) < 1e-9 || userInput == "q") break;
             }
         }
     }
