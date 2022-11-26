68f7e887be9ffddc265b6c63b000b8658d9bfb58
Kostya <super_serega_2007@mail.ru>
2022-11-26 22:38:00 +0500

    5.1

--- 5.1/Program.cs
+++ 5.1/Program.cs
@@ -29,36 +29,6 @@ namespace Hello
                         number = float.Parse(userInput);
                     }
                 }
-
-
-
-                /*if (isFirstInput)
-                {
-                    isFirstInput = false;
-                    float.TryParse(userInput, out currentNum);
-                    previousNum = currentNum - 1;
-                }
-                else
-                {
-                    if(float.TryParse(userInput, out _))
-                    {
-                        previousNum = currentNum;
-                        currentNum = float.Parse(userInput);
-                    }
-                }
-
-                if (int.TryParse(userInput, out int b))
-                {
-                    Console.WriteLine(b.ToString());
-                }
-                
-                if (!int.TryParse(userInput, out _) || userInput == "q")
-                {
-                    if (Math.Abs(previousNum - currentNum) < 1e-9 || userInput == "q")
-                    {
-                        break;
-                    }
-                }*/
             }
         }
     }
